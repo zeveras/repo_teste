0426eddcd60608ea7be4e77700feef6a33907011
zeveras <joserveras@gmail.com>
2013-08-03 17:58:20 -0300

    First commit.

---
+++ alpha/src/main/java/br/com/alpha/servlet/ShowTable.java
@@ -0,0 +1,26 @@
+package br.com.alpha.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShowTable extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5040175549102347356L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>olá! teste!</body></html>");
+		
+		throw new IOException();
+	}
+}
